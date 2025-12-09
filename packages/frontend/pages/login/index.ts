f38892c4d5364f375e0d7a2d52c23e44f9e1fcab
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-12-09 21:58:26 +0300

    Enhance login page functionality: add event listener for form submission, preventing default behavior and logging email and password inputs for debugging. Ensure newline at end of file in cold lead register page for consistency.

--- packages/frontend/pages/login/index.ts
+++ packages/frontend/pages/login/index.ts
@@ -1,1 +1,12 @@
+import { queryElement } from '$utils/selectors';
+
 console.log('Logidvsn page');
+const form = document.querySelector('[dev-target="login-form"]');
+form?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const email = queryElement<HTMLInputElement>('input[name="email"]', form!);
+  const password = queryElement<HTMLInputElement>(
+    '[dev-target="login-form"] input[name="password"]'
+  );
+  console.log(email, password);
+});
