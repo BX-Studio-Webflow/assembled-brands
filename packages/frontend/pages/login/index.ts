4ebe76e7e5f5948162b894eb19e8b3f98b3ebb55
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-12-09 22:16:25 +0300

    Refactor login page: implement comprehensive form validation for email and password inputs, including format checks and length requirements. Enhance error handling and logging for improved user feedback during registration process.

--- packages/frontend/pages/login/index.ts
+++ packages/frontend/pages/login/index.ts
@@ -1,12 +1,54 @@
+import type { AxiosError } from 'axios';
+import { apiColdLeadRegister } from 'shared/services/AuthService';
+
+import { isValidEmail } from '$utils/helpers';
 import { queryElement } from '$utils/selectors';
 
-console.log('Logidvsn page');
-const form = document.querySelector('[dev-target="login-form"]');
-form?.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const email = queryElement<HTMLInputElement>('input[name="email"]', form!);
-  const password = queryElement<HTMLInputElement>(
-    '[dev-target="login-form"] input[name="password"]'
-  );
-  console.log(email, password);
-});
+const initLoginPage = () => {
+  console.log('Log page');
+  const form = document.querySelector('[dev-target="login-form"]');
+  if (!form) {
+    console.error('Login form not found');
+    return;
+  }
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const email = queryElement<HTMLInputElement>('[dev-target="email-input"]', form);
+    const password = queryElement<HTMLInputElement>('[dev-target="password-input"]', form);
+    if (!email || !password) {
+      console.error('Email or password input not found');
+      return;
+    }
+    //validate email
+    if (!email.value) {
+      console.error('Invalid email');
+      return;
+    }
+    //validate password
+    if (!password.value) {
+      console.error('Invalid password');
+      return;
+    }
+    //validate password length
+    if (password.value.length < 8) {
+      console.error('Password must be at least 8 characters long');
+      return;
+    }
+    //validate email format
+    if (!isValidEmail(email.value)) {
+      console.error('Invalid email format');
+      return;
+    }
+    try {
+      apiColdLeadRegister({
+        work_email: email.value,
+      });
+    } catch (error) {
+      const { message } = error as AxiosError;
+      console.error(message);
+    }
+  });
+};
+
+initLoginPage();
--- packages/frontend/shared/utils/helpers.ts
+++ packages/frontend/shared/utils/helpers.ts
@@ -16,3 +16,15 @@ export const greetUser = (name: string) => {
     })}.`
   );
 };
+
+export const isValidEmail = (email: string) => {
+  if (!email) {
+    return false;
+  }
+  //check if email is valid regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return false;
+  }
+  return true;
+};
